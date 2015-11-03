9d80909359d4c8915d2ae0a4ed34042085757e93
Chirayu Desai <chirayudesai1@gmail.com>
2015-11-03 14:59:00 +0000

    flamingo: Improve MAC address handling
    Replace script with macaddrsetup
    * Replace it with Sony AOSP's macaddrsetup,
      which links with libta (a sony proprietary library)
      and passes the mac address to the wlan driver directly.
    * BT MAC handling also improved.
    
    * macaddrsetup comes from:
        https://github.com/sonyxperiadev/macaddrsetup
    
    Change-Id: Ic921198fa7278f66a991a7603f3c963c0b27fcc5

--- device.mk
+++ device.mk
@@ -75,6 +75,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstlport
 
+# WiFi and Bluetooth MAC addresses
+PRODUCT_PACKAGES += \
+    macaddrsetup
+
 PRODUCT_AAPT_CONFIG := large
 PRODUCT_AAPT_PREBUILT_DPI := hdpi
 PRODUCT_AAPT_PREF_CONFIG := hdpi

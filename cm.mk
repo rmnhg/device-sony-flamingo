25c849e2c53c9fe977bd48e2732c79cc2b48eaa7
Adam Farden <adam@farden.cz>
2016-12-26 22:41:20 +0100

    flamingo: Initial config for CyanogenMod

---
+++ cm.mk
@@ -0,0 +1,25 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT	:= 854
+TARGET_SCREEN_WIDTH	:= 480
+
+TARGET_KERNEL_CONFIG := arima_8926ss_ap_defconfig
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/flamingo/device.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Fingerprint for flamingo (from stock)
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D2203
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D2203/D2203:4.4.4/18.5.C.0.19/7H9_bQ:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D2203-user 4.4.4 18.5.C.0.19 7H9_bQ release-keys"
+
+# Override Product Name for CyanogenMod
+PRODUCT_NAME		:= cm_flamingo
+PRODUCT_DEVICE		:= flamingo
+PRODUCT_MODEL		:= Xperia E3
+PRODUCT_BRAND		:= Sony
+PRODUCT_MANUFACTURER	:= Sony

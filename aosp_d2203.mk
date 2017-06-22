d8de01549b102951812af14052796d08b1ae8b07
Ramón Hernández García <rmnhg16@gmail.com>
2017-06-22 22:59:52 +0200

    Update defconfig name

--- aosp_d2203.mk
+++ aosp_d2203.mk
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_KERNEL_CONFIG := aosp_yukon_flamingo_defconfig
-
+TARGET_KERNEL_CONFIG := arima_8926ss_ap_defconfig
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/flamingo/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)

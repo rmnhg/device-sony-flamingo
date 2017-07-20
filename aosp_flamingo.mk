dcdfc616686f64235f7e82949a5ba099564a4e60
Ramón Hernández García <rmnhg16@gmail.com>
2017-07-20 03:31:26 +0200

    New changes for Pure Nexus
    New peoperties and removed missing vendor folder

--- aosp_flamingo.mk
+++ aosp_flamingo.mk
@@ -12,7 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Boot animation
+TARGET_SCREEN_HEIGHT	:= 854
+TARGET_SCREEN_WIDTH	:= 480
+
+#Kernel config
 TARGET_KERNEL_CONFIG := arima_8926ss_ap_defconfig
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/flamingo/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
@@ -27,5 +33,9 @@ PRODUCT_MANUFACTURER := Sony
 PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/sony/flamingo/device.mk)
-$(call inherit-product-if-exists, vendor/sony/yukon-flamingo/flamingo-vendor.mk)
 $(call inherit-product-if-exists, vendor/sony/flamingo/flamingo-vendor.mk)
+
+# Fingerprint for flamingo (from stock)
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D2203
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D2203/D2203:4.4.4/18.5.C.0.19/7H9_bQ:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D2203-user 4.4.4 18.5.C.0.19 7H9_bQ release-keys"

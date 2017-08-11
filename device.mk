d48563a391729e73e21afcd3125d4419fc47b476
Ramón Hernández García <rmnhg16@gmail.com>
2017-08-11 14:24:58 +0200

    Temp: remove vendor/gapps

--- device.mk
+++ device.mk
@@ -65,4 +65,4 @@ PRODUCT_PROPERTY_OVERRIDES := \
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/yukon/platform.mk)
 $(call inherit-product, vendor/sony/flamingo/flamingo-vendor.mk)
-$(call inherit-product, vendor/google/build/opengapps-packages.mk)
+# $(call inherit-product, vendor/google/build/opengapps-packages.mk)

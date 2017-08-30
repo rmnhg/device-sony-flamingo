9724100942524fb7c4db6aa10de9be878f86e58e
Ramón Hernández García <rmnhg16@gmail.com>
2017-08-31 01:42:37 +0200

    Add common-headers

--- Android.mk
+++ Android.mk
@@ -2,6 +2,7 @@ ifeq ($(filter-out flamingo,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
+include device/sony/common-headers/KernelHeaders.mk
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif

dd3a58f60e51d1479995ff502aafc61cc4e43eb5
Ramón Hernández García <rmnhg16@gmail.com>
2017-09-06 19:59:46 +0200

    Remove common-headers

--- Android.mk
+++ Android.mk
@@ -2,7 +2,6 @@ ifeq ($(filter-out flamingo,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
-include device/sony/common-headers/KernelHeaders.mk
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif

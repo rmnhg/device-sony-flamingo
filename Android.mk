1379d1acb53fe63ca014060d7b86f366d8c93670
Ramón Hernández <rmnhg16@gmail.com>
2018-05-15 19:19:46 +0200

    flamingo: define board path Also mark $DEVICE_PATH on device/sony/flamingo Based on Huashan Sony's device
    Change-Id: Iba2506849552236f45e02149349892005433d735

--- Android.mk
+++ Android.mk
@@ -3,5 +3,4 @@ ifeq ($(filter-out flamingo,$(TARGET_DEVICE)),)
 LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
 endif
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright 2017 The Android Open Source Project
+# Copyright 2018 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,11 @@
 
 include device/sony/yukon/PlatformConfig.mk
 
-TARGET_SPECIFIC_HEADER_PATH += device/sony/flamingo/include
+# Device path
+DEVICE_PATH := device/sony/flamingo
+
+# Device board elements
+include $(DEVICE_PATH)/board/*.mk
+
+# Device headers 
+TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include

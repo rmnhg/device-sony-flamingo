474063e0224d84c24524d87bb490e4e62d8fa17c
Ramón Hernández García <rmnhg16@gmail.com>
2018-11-29 20:27:49 +0000

    Still some updates were needed I know I'm a bit dirty for these commits :/

--- full_flamingo.mk
+++ full_flamingo.mk
@@ -27,7 +27,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/sony/common/resources.mk)
 
 # Inherit from flamingo device
-$(call inherit-product, device/sony/flamingo/flamingo.mk)
+$(call inherit-product, device/sony/flamingo/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_flamingo
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_flamingo-eng
+add_lunch_combo lineage_flamingo-userdebug
